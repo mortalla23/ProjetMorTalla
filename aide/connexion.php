1f1159f5cf65fb4859bad303f4447a43f48df057
mor <mor.talla@groupe-esigelec.org>
2023-11-13 10:50:28 +0100

    quelques changement

---
+++ aide/connexion.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+    $titre = "Connexion";
+    include 'header.inc.php';
+    include 'menu.inc.php';
+?>
+<div class="container">
+<h1>Connexion</h1>
+    <p>À faire...</p>
+</div>
+<?php
+    include 'footer.inc.php';
+?>
---
+++ connpdo.php
@@ -0,0 +1,12 @@
+<?php 
+try 
+{
+$strConnection='mysql:host=localhost;dbname=espacemembres';
+$pdo=new PDO($strConnection,'root','');
+
+}catch (PDOException $e)
+{
+$msg='ERROR PDO ON '. $e->getMessage();
+die($msg);
+}
+?>
